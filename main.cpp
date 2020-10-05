192236188fb2e6fcd4f7388e5a7d0593022e63f1
peter <1205422966@qq.com>
2020-10-05 21:49:26 +0800

    multi compiler & os

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <kerbal/container/list.hpp>
+#include <kerbal/random/mersenne_twister_engine.hpp>
+
 #include <iostream>
 
 int main()
 {
-	kerbal::container::list<int> l;
-	for (int i = 0; i < 10; ++i) {
-		l.push_back(i);
+	kerbal::random::mt19937 eg;
+	kerbal::container::list<int> l = KERBAL_ILIST(0, 1, 2, 3);
+
+	for (int i = 0; i < 100; ++i) {
+		l.push_back(eg());
 	}
+
+	l.sort();
+
 	for (auto e : l) {
 		std::cout << e << std::endl;
 	}
